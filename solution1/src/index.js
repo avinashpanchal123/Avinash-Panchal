be885fd4dcfacadca72d796560ab720bc80a597e
Avinash Panchal <88651615+avinashpanchal123@users.noreply.github.com>
2022-04-18 18:42:22 +0530

    created prime route getting correct output

--- solution1/src/index.js
+++ solution1/src/index.js
@@ -2,6 +2,39 @@ const express = require("express");
 
 const app = express();
 
+function primes(){
+    let res = [];
+    for(let i = 2; i < 30; i++){
+        let flag = true;
+        for( let j = 2; j < i; j++){
+            if( i % j === 0){
+                flag = false
+                break;
+            }
+        }
+        if( flag == true){
+            res.push(i)
+        }
+    }
+    console.log(res)
+
+    let output = {
+        primes: res
+    }
+    return (output)
+}
+
+
+
+
+
+app.get("/primes", (req, res)=>{
+    const Primes = primes()
+
+    res.send(Primes)
+
+})
+
 
 
 app.get("/", (req, res)=>{
