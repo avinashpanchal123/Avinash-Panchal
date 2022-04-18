2aaa55e930539b5efe0cee1b4942971ac9097f5f
Avinash Panchal <88651615+avinashpanchal123@users.noreply.github.com>
2022-04-18 18:47:28 +0530

    created fibs route and getting first 8 fibonacci  numbers

--- solution1/src/index.js
+++ solution1/src/index.js
@@ -25,13 +25,52 @@ function primes(){
 }
 
 
+let dp = [];
+dp[0] = 0
+dp[1] = 1;
 
+function fibonacci(n){
+    if( n == 0){
+        return  dp[0] 
+    }
+    if( n == 1){
+      
+        return dp[1]
+    }
+    if( dp[n] == undefined){
+        dp[n] = fibonacci(n-1)+fibonacci(n-2)
+        return dp[n]
+    }
+    else{
+        return dp[n]
+    }
+    
+}
+
+
+let fib = fibonacci(8);
+console.log(dp)
+
+const Primes = primes()
+
+console.log(Primes)
 
 
 app.get("/primes", (req, res)=>{
     const Primes = primes()
 
     res.send(Primes)
+
+})
+
+app.get("/fib", (req, res)=>{
+    let fib = fibonacci(8);
+
+    let output = {
+        fibs : dp
+    }
+
+    res.send(output)
 
 })
 
